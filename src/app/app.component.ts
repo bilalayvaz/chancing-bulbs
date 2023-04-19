eb1a8f275bc68cd85e247e2ca6007f3e307668c2
Bilal AYVAZOĞLU <bilalayvazoglu@gmail.com>
2023-04-19 14:39:33 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'chancing-bulbs';
+  counter: number = 0;
+  passIsOver: boolean = false;
+  greenBulbs: number[] = [];
+  bulbs: Array<{ id: number; isOpen: boolean }> = [];
+
+  ngOnInit() {
+    for (let i = 0; i < 100; i++) {
+      this.bulbs.push({ id: i + 1, isOpen: false });
+    }
+  }
+
+  changeBulbs() {
+    this.counter++;
+
+    for (let i = 0; i < 100; i++) {
+      if (this.bulbs[i].id % this.counter === 0) {
+        this.bulbs[i] = { id: i + 1, isOpen: !this.bulbs[i].isOpen };
+      } else {
+        this.bulbs[i] = { id: i + 1, isOpen: this.bulbs[i].isOpen };
+      }
+    }
+
+    if (this.counter === 100) {
+      this.passIsOver = true;
+
+      for (let i = 0; i < 100; i++) {
+        if (this.bulbs[i].isOpen === true) {
+          this.greenBulbs.push(this.bulbs[i].id);
+        }
+      }
+    }
+  }
+}
